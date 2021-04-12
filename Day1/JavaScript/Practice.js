466ec265c83bf4492b9d3edaa188c9e2c26d409b
sandesh578 <66244625+sandesh578@users.noreply.github.com>
2021-04-12 18:26:24 +0530

    Add files via upload

---
+++ Day1/JavaScript/Practice.js
@@ -0,0 +1,28 @@
+console.log("Hello world");
+
+//single line comment
+
+/*
+ Multiline Comment
+ */
+
+//variable declaration
+var x = 5; //integer
+var name = "Sandesh Parajuli" //string
+var ch = 'c' //character
+var gpa = 9.7; //double
+
+//printing variable to console
+console.log(x);
+console.log(name);
+console.log(ch);
+console.log(gpa);
+
+/*
+Output:
+Hello world
+5
+Sandesh Parajuli
+c
+9.7
+*/
